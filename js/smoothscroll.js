79f893ee896454613db1dc9cf2ef24bd727e8e93
jumin20099 <jumin20099@gmail.com>
2024-10-31 15:56:37 +0900

    주석 수정

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,20 +1,20 @@
-// Initializes smooth scrolling with Lenis and integrates it with GSAP's ScrollTrigger.
-// Function to set up smooth scrolling.
+// Lenis를 사용하여 부드러운 스크롤을 초기화하고 GSAP의 ScrollTrigger와 통합합니다.
+// 부드러운 스크롤을 설정하는 함수
 const initSmoothScrolling = () => {
-  // Initialize Lenis for smooth scroll effects. Lerp value controls the smoothness.
+  // 부드러운 스크롤 효과를 위한 Lenis 초기화. Lerp 값은 부드러움을 조절합니다.
   const lenis = new Lenis({ lerp: 0.15 });
   
-  // Sync ScrollTrigger with Lenis' scroll updates.
+  // Lenis의 스크롤 업데이트와 ScrollTrigger를 동기화합니다.
   lenis.on('scroll', ScrollTrigger.update);
   
-  // Ensure GSAP animations are in sync with Lenis' scroll frame updates.
+  // GSAP 애니메이션이 Lenis의 스크롤 프레임 업데이트와 동기화되도록 합니다.
   gsap.ticker.add(time => {
-      lenis.raf(time * 1000); // Convert GSAP's time to milliseconds for Lenis.
+      lenis.raf(time * 1000); // GSAP의 시간을 밀리초로 변환하여 Lenis에 전달합니다.
   });
   
-  // Turn off GSAP's default lag smoothing to avoid conflicts with Lenis.
+  // Lenis와의 충돌을 피하기 위해 GSAP의 기본 지연 부드럽게 설정을 끕니다.
   gsap.ticker.lagSmoothing(0);
 };
 
-// Activate the smooth scrolling feature.
+// 부드러운 스크롤 기능 활성화
 initSmoothScrolling();
--- js/utils.js
+++ js/utils.js
@@ -1,17 +1,17 @@
 /**
- * Preloads images specified by the CSS selector.
+ * CSS 선택자로 지정된 이미지를 미리 로드합니다.
  * @function
- * @param {string} [selector='img'] - CSS selector for target images.
- * @returns {Promise} - Resolves when all specified images are loaded.
+ * @param {string} [selector='img'] - 대상 이미지에 대한 CSS 선택자.
+ * @returns {Promise} - 지정된 모든 이미지가 로드되면 해결됩니다.
  */
 const preloadImages = (selector = 'img') => {
   return new Promise((resolve) => {
-      // The imagesLoaded library is used to ensure all images (including backgrounds) are fully loaded.
+      // imagesLoaded 라이브러리를 사용하여 모든 이미지(배경 포함)가 완전히 로드되었는지 확인합니다.
       imagesLoaded(document.querySelectorAll(selector), {background: true}, resolve);
   });
 };
 
-// Exporting utility functions for use in other modules.
+// 다른 모듈에서 사용할 수 있도록 유틸리티 함수를 내보냅니다.
 export {
   preloadImages
 };
